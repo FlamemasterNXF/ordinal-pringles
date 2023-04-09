d76a84537ca1c771aacfe527bc2811851ba5f55e
FlamemasterNXF <flamemasternxf@gmail.com>
2023-04-09 15:53:25 -0400

    feat: Improved BP UI, OC UI

--- src/overflow.js
+++ src/overflow.js
@@ -2,9 +2,20 @@ function updateOverflowHTML(){
     DOM(`boosterText2`).innerText =  `You have ${getExtraBoosters()} Excess Boosters, producing`
     DOM(`boosterPower`).innerText = ` 0 Booster Power/s`
     DOM(`bpTotal`).innerText = `Your ${format(data.overflow.bp)} Booster Power is`
+
+    DOM(`chargeText2`).innerText =  `You have ${getExtraCharge()} Excess Charge, producing`
+    DOM(`overCharge`).innerText = ` 0 Overcharge/s`
+    DOM(`ocTotal`).innerText = `Your ${format(data.overflow.oc)} Overcharge is`
+
+    for (let i = 0; i < 5; i++) {
+        DOM(`bp${i}Effect`).innerText = i==2 ? `/${getOverflowEffect()}` : `${getOverflowEffect()}x`
+    }
 }
 
-const overflowEffectRequirements = [1e3, 1e10, 1e100]
-
 let maxNonOverflowBoosters = boostersAtGivenFB(27)
-let getExtraBoosters = () => data.boost.total-maxNonOverflowBoosters
+let getExtraBoosters = () => Math.max(0, data.boost.total-maxNonOverflowBoosters)
+let getExtraCharge = () => Math.max(0, data.incrementy.charge-12)
+
+function getOverflowEffect(){
+    return 2
+}
