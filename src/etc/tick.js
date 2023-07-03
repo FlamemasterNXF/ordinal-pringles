1050fd4e094a7b411ec87b94922248e740e5d3f4
Braeden Lotterman <60369767+FlamemasterNXF@users.noreply.github.com>
2023-07-03 09:50:18 -0400

    fix: Reference Error

--- src/etc/tick.js
+++ src/etc/tick.js
@@ -59,7 +59,7 @@ function tick(diff){
 
     // Automation Tier 3
     let inSluggish = false
-    if (data.boost.times === 2 && !hasSluggish[4]) inSluggish = true
+    if (data.boost.times === 2 && !data.collapse.hasSluggish[4]) inSluggish = true
     if(data.collapse.hasSluggish[3] && data.collapse.apEnabled[0] && data.ord.base > 3) factorShift()
     if(data.collapse.hasSluggish[3] && data.collapse.apEnabled[1] && data.boost.times < boostTimesLimit && !inSluggish) boost(false, true)
 
