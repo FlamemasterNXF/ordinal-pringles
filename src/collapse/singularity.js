772701bd3e7f369c153ec6b6c92cb1eb6e74e79e
FlamemasterNXF <flamemasternxf@gmail.com>
2023-07-10 20:31:38 -0400

    feat: Refactor Singularity to cost Charge

--- src/collapse/singularity.js
+++ src/collapse/singularity.js
@@ -1,10 +1,10 @@
 function updateSingularityHTML(){
-    DOM(`singAutoText`).innerHTML = `Your <span style="color: #80ceff">Markup AutoBuyer</span> speed is <span style="color: #8080FF">${format(t2Auto())}`
+    DOM(`singCostText`).innerHTML = `You have <span style="color: goldenrod">${data.incrementy.charge} Charge</span>`
 }
 function updateSingLevelHTML(){
     DOM(`singLevel`).innerHTML = `Your Singularity has a density of <b>${data.sing.level > 0 ? ordinalDisplay('H', data.sing.level, 0, 10, 3, false) : `H<sub>0</sub>`}</b> (10)`
     DOM(`singLevel2`).innerHTML = `Your Singularity's highest ever density was <b>${data.sing.highestLevel > 0 ? ordinalDisplay('H', data.sing.highestLevel, 0, 10, 3, false) : `H<sub>0</sub>`}</b> (10)`
-    DOM(`singEffect`).innerHTML = `Your Singularity is raising Cardinal Gain to the <b>${format(mainSingEffect(), 3)}</b> and dividing the Markup Autobuyer's speed by ${format(singAutoEffect())}`
+    DOM(`singEffect`).innerHTML = `Your Singularity is raising Cardinal Gain to the <b>${format(mainSingEffect(), 3)}</b>`
 }
 
 function loadSingularityHTML(){
@@ -14,15 +14,21 @@ function loadSingularityHTML(){
 }
 
 let mainSingEffect = () => 1 + Math.sqrt(data.sing.level)/1000
-let singAutoEffect = () => data.sing.level > 0 ? Math.max(2**data.sing.level) : 1
-let maxSingLevel = () => Math.log2(t2AutoPure())
+let maxSingLevel = () => data.incrementy.charge
+
 
 function changeSingLevel(single = false){
-    DOM(`singSlider`).max = Math.max(1, maxSingLevel())
+    DOM(`singSlider`).max = Math.max(1, data.incrementy.charge+data.sing.level)
 
-    if(data.sing.level + 1 > maxSingLevel() && single)
-        return createAlert('I wouldn\'t recommend that!', 'If you grow your Singularity any further you will destroy your Markup AutoBuyer!', 'Oops!')
-    data.sing.level = single ? data.sing.level + 1 : parseInt(DOM(`singSlider`).value)
+    const change = single ? data.sing.level + 1 : parseInt(DOM(`singSlider`).value)
+    const cost = change-data.sing.level
+    if(single && data.incrementy.charge === 0) return createAlert('Failure', 'Insufficient Charge!', 'Oops')
+    if(!single && data.incrementy.charge - cost < 0) return createAlert('Failure', 'Insufficient Charge!', 'Oops')
+
+    if(single) --data.incrementy.charge
+    if(!single && data.sing.level > change) data.incrementy.charge += data.sing.level-change
+    if(!single && data.sing.level < change) data.incrementy.charge -= cost
+    data.sing.level = change
 
     if(data.sing.level > data.sing.highestLevel) data.sing.highestLevel = data.sing.level
 
@@ -30,13 +36,16 @@ function changeSingLevel(single = false){
 }
 
 function singControl(i){
-    if(i === 0) data.sing.level = data.sing.highestLevel
-    if(i === 1){
-        data.sing.level = data.sing.level = maxSingLevel()
+    if(i === 0){
+        data.sing.level = maxSingLevel()
+        data.incrementy.charge = 0
         if(data.sing.level > data.sing.highestLevel) data.sing.highestLevel = data.sing.level
     }
-    if(i === 2) data.sing.level = 0
-    if(i === 3) changeSingLevel(true)
+    if(i === 1){
+        data.incrementy.charge += data.sing.level
+        data.sing.level = 0
+    }
+    if(i === 2) changeSingLevel(true)
     updateSingLevelHTML()
     DOM(`singSlider`).value = data.sing.level
 }
