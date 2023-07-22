cb0bf09f131c361f0a09500f72da948dae1c61ce
FlamemasterNXF <flamemasternxf@gmail.com>
2023-07-21 22:54:03 -0400

    chore: Version Bump, Remove Beta Flag

--- src/data.js
+++ src/data.js
@@ -7,10 +7,10 @@ const GRAHAMS_VALUE = 109
 const BHO_VALUE = 4*3**40
 
 //Version Flags
-const VERSION = "0.2b4"
+const VERSION = "0.2"
 const VERSION_NAME = "The Baseless Pringularity"
 const VERSION_DATE = "July 21st, 2023"
-const IS_BETA = true
+const IS_BETA = false
 const SAVE_PATH = () => IS_BETA ? "ordinalPRINGLESBETAsave" : "ordinalPRINGLESsave"
 
 //create all the variables in a data object for saving
