30bd94f366a3cc7d2212ec0ab4a6c17ccecd3af1
unsoftcapped3 <75136164+unsoftcapped3@users.noreply.github.com>
2023-08-22 22:04:18 -0400

    try to fix hierarchy buyable cost

--- src/internal/FormatOrd.js
+++ src/internal/FormatOrd.js
@@ -202,19 +202,18 @@ function numberFromOrdinal(string, base) {
 }
 
 function displayHierarchyOrd(ord,over,base,trim = data.ord.trim) {
-    if (ord === Infinity) return data.gword ? "<img src='https://cdn.discordapp.com/emojis/967188082434662470.webp?size=24'>" : "Ω"
-    ord = Math.floor(ord)
-    over = Math.floor(over)
+    ord = Decimal.floor(ord)
+    over = Decimal.floor(over)
     if(trim <= 0) return `...`
-    if(ord < base) return ord+over
-    const magnitude = Math.floor(Math.log(ord)/Math.log(base)+1e-14)
-    const magnitudeAmount = base**magnitude
-    const amount = Math.floor(ord/magnitudeAmount)
+    if(ord < base) return ord.plus(over)
+    const magnitude = Decimal.floor(Decimal.ln(ord)/Decimal.ln(base).plis(D(1e-14)))
+    const magnitudeAmount = D(base).pow(magnitude)
+    const amount = Decimal.floor(ord.div(magnitudeAmount))
     let finalOutput = "&omega;"
-    if (magnitude > 1) finalOutput += "<sup>"+displayHierarchyOrd(magnitude, 0, base)+"</sup>"
-    if (amount > 1) finalOutput += amount
-    const firstAmount = amount*magnitudeAmount
-    if(ord-firstAmount > 0) finalOutput += "+" + displayHierarchyOrd(ord-firstAmount, over, base, trim - 1)
+    if (magnitude.gt(1)) finalOutput += "<sup>"+displayHierarchyOrd(magnitude, 0, base)+"</sup>"
+    if (amount.gt(1)) finalOutput += amount
+    const firstAmount = amount.times(magnitudeAmount)
+    if(ord.sub(firstAmount).gt(0)) finalOutput += "+" + displayHierarchyOrd(ord.sub(firstAmount), over, base, trim - 1)
     if(data.gword) finalOutput = finalOutput.replaceAll("&omega;","<img src='https://cdn.discordapp.com/emojis/853002327362895882.webp?size=24'>")
                                             .replaceAll("ω","<img src='https://cdn.discordapp.com/emojis/853002327362895882.webp?size=24'>")
     return finalOutput
