4ba719ec905c556166d6e3aaa183ea95c4b0fd2c
Braeden <flamemasternxf@gmail.com>
2023-11-27 17:57:31 -0500

    feat: Expand OrdMarks up to FB156,765,267,918,903

--- src/internal/FormatOrd.js
+++ src/internal/FormatOrd.js
@@ -138,6 +138,168 @@ function makeExcessOrdMarks(){
     ordMarks.push("&psi;(Ω<sub>2</sub><sup>Ω</sup>&psi;<sub>1</sub>(Ω<sub>2</sub><sup>2</sup>))")
 }
 
+// CREDIT TO https://ordinal-pringles-dark-mode.glitch.me/
+
+const ordMarksX = [
+    "&psi;(Ω<sub>2</sub>x)",
+    "&psi;(Ω<sub>2</sub><sup>2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω2+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω2+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω2+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ωy</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup></sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω2+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω2+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ω2+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>+Ωy</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω2+1</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω2+2</sup>x)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ω2+y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>2+Ωy</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>2</sup>y</sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>y</sup></sup>)",
+    "&psi;(Ω<sub>2</sub><sup>Ω<sup>Ω</sup></sup>)",
+];
+
+const ordMarksXStart = [
+    40, // Ω₂
+    163, // Ω₂²
+    532, // Ω₂^ω
+    533, // Ω₂^Ω
+    1643, // Ω₂^(Ω+1)
+    4973, // Ω₂^(Ω+2)
+    14963, // Ω₂^(Ω+ω)
+    14964, // Ω₂^(Ω2)
+    44937, // Ω₂^(Ω2+1)
+    134856, // Ω₂^(Ω2+2)
+    404613, // Ω₂^(Ω2+ω)
+    404614, // Ω₂^(Ωω)
+    404615, // Ω₂^(Ω²)
+    1213892, // Ω₂^(Ω²+1)
+    3641723, // Ω₂^(Ω²+2)
+    10925216, // Ω₂^(Ω²+ω)
+    10925217, // Ω₂^(Ω²+Ω)
+    32775699, // Ω₂^(Ω²+Ω+1)
+    98327145, // Ω₂^(Ω²+Ω+2)
+    294981483, // Ω₂^(Ω²+Ω+ω)
+    294981484, // Ω₂^(Ω²+Ω2)
+    884944501, // Ω₂^(Ω²+Ω2+1)
+    2654833552, // Ω₂^(Ω²+Ω2+2)
+    7964500705, // Ω₂^(Ω²+Ω2+ω)
+    7964500706, // Ω₂^(Ω²+Ωω)
+    7964500707, // Ω₂^(Ω²2)
+    23893502172, // Ω₂^(Ω²2+1)
+    71680506567, // Ω₂^(Ω²2+2)
+    215041519752, // Ω₂^(Ω²2+ω)
+    215041519753, // Ω₂^(Ω²2+Ω)
+    645124559311, // Ω₂^(Ω²2+Ω+1)
+    1935373677985, // Ω₂^(Ω²2+Ω+2)
+    5806121034007, // Ω₂^(Ω²2+Ω+ω)
+    5806121034008, // Ω₂^(Ω²2+Ω2)
+    17418363102077, // Ω₂^(Ω²2+Ω2+1)
+    52255089306284, // Ω₂^(Ω²2+Ω2+2)
+    156765267918905, // Ω₂^(Ω²2+Ω2+ω)
+    156765267918906, // Ω₂^(Ω²2+Ωω)
+    156765267918907, // Ω₂^(Ω²ω)
+    156765267918908, // Ω₂^(Ω^ω)
+    156765267918909, // Ω₂^(Ω^Ω)
+];
+
+const ordMarksXLength = [
+    41, // Ω₂
+    123, // Ω₂²
+    369, // Ω₂^ω
+    370, // Ω₂^Ω
+    1110, // Ω₂^(Ω+1)
+    3330, // Ω₂^(Ω+2)
+    9990, // Ω₂^(Ω+ω)
+    9991, // Ω₂^(Ω2)
+    29973, // Ω₂^(Ω2+1)
+    89919, // Ω₂^(Ω2+2)
+    269757, // Ω₂^(Ω2+ω)
+    269758, // Ω₂^(Ωω)
+    269759, // Ω₂^(Ω²)
+    809277, // Ω₂^(Ω²+1)
+    2427831, // Ω₂^(Ω²+2)
+    7283493, // Ω₂^(Ω²+ω)
+    7283494, // Ω₂^(Ω²+Ω)
+    21850482, // Ω₂^(Ω²+Ω+1)
+    65551446, // Ω₂^(Ω²+Ω+2)
+    196654338, // Ω₂^(Ω²+Ω+ω)
+    196654339, // Ω₂^(Ω²+Ω2)
+    589963017, // Ω₂^(Ω²+Ω2+1)
+    1769889051, // Ω₂^(Ω²+Ω2+2)
+    5309667153, // Ω₂^(Ω²+Ω2+ω)
+    5309667154, // Ω₂^(Ω²+Ωω)
+    5309667155, // Ω₂^(Ω²2)
+    15929001465, // Ω₂^(Ω²2+1)
+    47787004395, // Ω₂^(Ω²2+2)
+    143361013185, // Ω₂^(Ω²2+ω)
+    143361013186, // Ω₂^(Ω²2+Ω)
+    430083039558, // Ω₂^(Ω²2+Ω+1)
+    1290249118674, // Ω₂^(Ω²2+Ω+2)
+    3870747356022, // Ω₂^(Ω²2+Ω+ω)
+    3870747356023, // Ω₂^(Ω²2+Ω2)
+    11612242068069, // Ω₂^(Ω²2+Ω2+1)
+    34836726204207, // Ω₂^(Ω²2+Ω2+2)
+    104510178612621, // Ω₂^(Ω²2+Ω2+ω)
+    104510178612622, // Ω₂^(Ω²2+Ωω)
+    104510178612623, // Ω₂^(Ω²ω)
+    104510178612624, // Ω₂^(Ω^ω)
+    104510178612625, // Ω₂^(Ω^Ω)
+];
+
+/*
+    Generates Ordinals up to Factor Boost FB156,765,267,918,903
+    That's an Ordinal Value of 3.3982083289425593e74796041325934
+/   If you ever need to expand this you're insane
+ */
+function infiniteOrdMarks(magnitude, layer = 0) {
+    if (D(magnitude).gte(ordMarksXStart[ordMarksXStart.length - 1])) return ordMarksX[ordMarksX.length - 1];
+    if (D(magnitude).floor().lt(ordMarks.length - 1) && !layer) return ordMarks[D(magnitude).floor().toNumber()];
+    magnitude = D(magnitude).add(0.000000000001).floor()
+    //console.log(magnitude + " " + layer)
+    if (!layer) {
+        let i = 0
+        while (i < ordMarksX.length - 1 && magnitude.add(0.000000000001).gte(ordMarksXStart[i + 1])) i++
+        let finalOutput = ordMarksX[i].replaceAll("x", infiniteOrdMarks(magnitude.sub(ordMarksXStart[i]), layer+1))
+        return `${finalOutput.replaceAll('undefined', '')}`
+    } else {
+        if (magnitude.lt(1)) return "x"
+        if (magnitude.lt(41)) return ordMarks[Decimal.floor(magnitude.add(0.000000000001)).toNumber()-1].split('(')[1].split(')')[0]
+        let i = 0
+        while (i < ordMarksX.length - 1 && magnitude.add(0.000000000001).gte(ordMarksXLength[i + 1])) i++
+        let finalOutput = ordMarksX[i].replaceAll("&psi;", "&psi;<sub>1</sub>").replaceAll("x", infiniteOrdMarks(magnitude.sub(ordMarksXLength[i]), layer+1))
+        return `${finalOutput.replaceAll('undefined', '')}`
+    }
+    return "gwa"; // you've been gwa-ed
+}
+
+// End Credit
+
 function displayOrd(ord,over,base,trim = data.ord.trim) {
     if(data.ord.isPsi) return displayPsiOrd(ord, trim)
     if(ord === data.ord.ordinal && ord.gt(Number.MAX_VALUE)) return displayInfiniteOrd(ord, over, base, trim)
@@ -264,7 +426,7 @@ function displayPsiOrd(ord, trim = data.ord.trim, base = data.ord.base) {
 }
 
 function displayInfinitePsiOrd(ord, trim = data.ord.trim, base = data.ord.base) {
-    if (D(ord).mag === Infinity || isNaN(D(ord).mag)) return data.gword ? "<img src='https://cdn.discordapp.com/emojis/967188082434662470.webp?size=24'>" : "Ω"
+    if (D(ord).mag === Infinity || isNaN(D(ord).mag) || base < 1) return data.gword ? "<img src='https://cdn.discordapp.com/emojis/967188082434662470.webp?size=24'>" : "Ω"
     ord = D(Decimal.floor(D(ord).add(0.000000000001)))
     if(ord.eq(BHO_VALUE)) {
         let finalOutput = "&psi;(Ω<sub>2</sub>)"
@@ -272,7 +434,7 @@ function displayInfinitePsiOrd(ord, trim = data.ord.trim, base = data.ord.base) 
             .replaceAll("Ω","<img src='https://cdn.discordapp.com/emojis/967188082434662470.webp?size=24'>")
         return `${finalOutput}`
     }
-    let maxOrdMarks = (D(3).pow(ordMarks.length-1)).times(4)
+    let maxOrdMarks = (D(3).pow(ordMarksXStart[ordMarksXStart.length-1])).times(4) //(D(3).pow(ordMarks.length-1)).times(4)
     if(D(ord).gt(maxOrdMarks)) {
         return displayInfinitePsiOrd(maxOrdMarks) + "x" + format(ord.div(maxOrdMarks),2)
     }
@@ -281,7 +443,7 @@ function displayInfinitePsiOrd(ord, trim = data.ord.trim, base = data.ord.base) 
     if(ord.lt(4)) return extraOrdMarks[ord]
     const magnitude = Decimal.floor(Decimal.ln(ord.div(4)).div(Decimal.ln(3)))
     const magnitudeAmount = D(4).times(Decimal.pow(3, magnitude))
-    let finalOutput = ordMarks[Decimal.min(magnitude,ordMarks.length-1)]
+    let finalOutput = infiniteOrdMarks(Decimal.min(magnitude,ordMarksXStart[ordMarksXStart.length-1])) //ordMarks[Decimal.min(magnitude,ordMarks.length-1)]
     if(finalOutput.includes("x"))finalOutput = finalOutput.replace(/x/, displayInfinitePsiOrd(ord.sub(magnitudeAmount), trim-1))
     if(finalOutput.includes("y"))finalOutput = finalOutput.replace(/y/, displayInfinitePsiOrd(ord.sub(magnitudeAmount).plus(1), trim-1))
     if(data.gword) finalOutput=finalOutput
