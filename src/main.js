6f89114e39d96b6b29205e060206384372cd0ed6
Braeden <flamemasternxf@gmail.com>
2024-01-10 15:13:14 -0500

    fix: Weird Bug?

--- src/main.js
+++ src/main.js
@@ -36,7 +36,8 @@ function mainLoop() {
     if (controls["i"].pressed) markup();
     if (controls["f"].pressed) { buyMaxFactor(); buyMaxAuto(); }
     if (controls["h"].pressed && !data.baseless.baseless) factorShift();
-    if (controls["h"].pressed && data.baseless.baseless) dynamicShift();    if (controls["b"].pressed) boost(false, false);
+    if (controls["h"].pressed && data.baseless.baseless) dynamicShift();
+    if (controls["b"].pressed) boost(false, false);
     if (controls["c"].pressed) collapseConfirm(true);
 
     // Update Achievements
