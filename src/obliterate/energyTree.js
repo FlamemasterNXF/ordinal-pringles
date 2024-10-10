288706a2fc6a84fcafbe06b2fcc929b1fde26781
Braeden <flamemasternxf@gmail.com>
2024-10-10 16:33:48 -0400

    fix: Passive Respec

--- src/obliterate/energyTree.js
+++ src/obliterate/energyTree.js
@@ -172,6 +172,6 @@ function getTotalEnergyInvested(){
         let ids = getFixedTreeNode(data.obliterate.energyUpgrades[i])
         total += energyUpgradeData[ids[0]][ids[1]].cost
     }
-    total += (data.purity.isUnlocked.slice(0,10).filter(i=>i).length - 3)
+    //total += (data.purity.isUnlocked.slice(0,10).filter(i=>i).length - 3)
     return total
 }
