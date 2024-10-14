619bc3c241005b1de7cc8142579b87cb5c3a6c83
Braeden <flamemasternxf@gmail.com>
2024-10-14 10:25:06 -0400

    chore: Version Bump

--- src/data.js
+++ src/data.js
@@ -8,9 +8,9 @@ const BHO_VALUE = 4*3**40
 const BO_VALUE = D('eee98235035280650.45') //Decimal.pow(3, ordMarksBO).mul(4)
 
 //Version Flags
-const VERSION = "0.4"
+const VERSION = "0.4.1"
 const VERSION_NAME = "The Pringle Update"
-const VERSION_DATE = "October 13th, 2024"
+const VERSION_DATE = "October 14th, 2024"
 const IS_BETA = false
 const SAVE_PATH = () => IS_BETA ? "ordinalPRINGLESBETAsave" : "ordinalPRINGLESsave"
 
