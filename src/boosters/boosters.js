4128dbbef001a67662e68d116ae58742daa2786b
Braeden <flamemasternxf@gmail.com>
2024-10-17 20:12:34 -0400

    chore: Upgrade Darkness Header

--- src/boosters/boosters.js
+++ src/boosters/boosters.js
@@ -268,6 +268,7 @@ function updateHeaderHTML(){
     el.style.display = data.chal.active.includes(true) || data.baseless.baseless || inAnyPurification() ? 'block' : 'none'
     el.innerText = inAnyPurification() ? `The ${purificationData[data.omega.whichPurification].alt} will be Purified`
         : data.baseless.baseless ? `You are in the ${baselessNames[data.baseless.mode]} Realm`
+        : data.darkness.darkened ? `You trapped in Challenge 8 and there is ${format(data.chal.decrementy)} Decrementy [${format(decrementyGain())}x/s]`
         : data.chal.active[7] ? `You are in Challenge 8 and there is ${format(data.chal.decrementy)} Decrementy and ${Math.max(1000-data.successorClicks,0)} clicks left` : `You are in Challenge ${data.chal.html+1}` + (data.chal.active[6] ? ` and there is ${Math.max(1000-data.successorClicks,0)} clicks left` : "")
 }
 
