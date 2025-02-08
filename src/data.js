e6aaab62f7e50363f1907f6409db18d50505c89e
Braeden Lotterman <flamemasternxf@gmail.com>
2025-02-07 22:52:02 -0500

    chore: Version Bump, Modal Styling

--- src/data.js
+++ src/data.js
@@ -8,9 +8,9 @@ const BHO_VALUE = 4*3**40
 const BO_VALUE = D('eee98235035280650.45') //Decimal.pow(3, ordMarksBO).mul(4)
 
 //Version Flags
-const VERSION = "0.4.2"
+const VERSION = "0.4.3"
 const VERSION_NAME = "The Pringle Update"
-const VERSION_DATE = "November 1st, 2024"
+const VERSION_DATE = "February 7th, 2025"
 const IS_BETA = false
 const SAVE_PATH = () => IS_BETA ? "ordinalPRINGLESBETAsave" : "ordinalPRINGLESsave"
 
