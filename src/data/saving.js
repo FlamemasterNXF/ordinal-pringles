240fa2b0690a3ad62475fdbc8115cd6519ae07a1
Braeden Lotterman <flamemasternxf@gmail.com>
2025-05-06 12:26:53 -0400

    chore: Version Date Bump

--- src/data/saving.js
+++ src/data/saving.js
@@ -1,7 +1,7 @@
 //Version Flags
 const VERSION = "0.4.4b4"
 const VERSION_NAME = "The Pringle Update"
-const VERSION_DATE = "May 5th, 2025"
+const VERSION_DATE = "May 6th, 2025"
 const IS_BETA = true
 const SAVE_PATH = () => IS_BETA ? "ordinalPRINGLESBETAsave" : "ordinalPRINGLESsave"
 
