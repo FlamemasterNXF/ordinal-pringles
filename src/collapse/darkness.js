fa734ffd76755c481a8ae655cb0187c9ab935588
Braeden Lotterman <flamemasternxf@gmail.com>
2025-09-21 17:10:06 -0400

    fix: SD Gain

--- src/collapse/darkness.js
+++ src/collapse/darkness.js
@@ -135,7 +135,7 @@ function updateDarknessResourcesHTML(){
 
 function getStableDecrementy(){
     const multipliers = getDepthUpgradeLevel(0)*getHyperchargeEffect(11)
-    return Decimal.floor(Decimal.log10(data.darkness.bestDecrementy)).times(multipliers).toNumber()
+    return Decimal.floor(Decimal.log10(data.darkness.bestDecrementy.plus(1))).times(multipliers+1).toNumber()
 }
 
 function getLight(){
